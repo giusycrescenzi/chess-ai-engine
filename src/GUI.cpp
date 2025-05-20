875dc04dfa659a7de73826138b8e3cb02e65ab13
Giuseppe Crescenzi <giuseppecrescenzi17p@gmail.com>
2025-05-20 14:48:51 +0200

    GUI class for later

---
+++ src/GUI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Giuseppe Crescenzi on 20/05/25.
+//
+
+#include "GUI.h"
---
+++ src/GUI.h
@@ -0,0 +1,16 @@
+//
+// Created by Giuseppe Crescenzi on 20/05/25.
+//
+
+#ifndef GUI_H
+#define GUI_H
+
+
+
+class GUI {
+
+};
+
+
+
+#endif //GUI_H
